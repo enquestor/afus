0da962cffea2420391ba6d3fa503624eddf728b7
Enquestor <allen@enquestor.com>
2023-04-06 04:33:35 +0800

    feature(server): rate limiting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Short, ShortModule } from './short';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -23,9 +25,19 @@ import { join } from 'path';
       entities: [Short],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
+    ThrottlerModule.forRoot({
+      ttl: parseInt(process.env.TTL || '60'),
+      limit: parseInt(process.env.LIMIT || '6'),
+    }),
     ShortModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
