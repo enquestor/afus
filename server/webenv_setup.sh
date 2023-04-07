c4de7e9aa6ef989371ec3fcfba7cec9aaa966d65
Enquestor <allen@enquestor.com>
2023-04-07 18:12:32 +0800

    feature(web): support html title customization

--- server/webenv_setup.sh
+++ server/webenv_setup.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-DEFAULT_AFUS_TITLE='Afus'
-DEFAULT_AFUS_SUBTITLE='A fucking url shortener.'
+USE_AFUS_TITLE='Afus'
+USE_AFUS_SUBTITLE='A fucking url shortener.'
 
 replace() {
+  # replace asset
   for file in ./web/assets/*.js; do
     sed -i.bak "s/$1/$2/g" "$file"
   done
+
+  # replace index
+  sed -i.bak "s/$1/$2/g" ./web/index.html
 }
 
 if [ -n "$AFUS_TITLE" ]; then
   echo Set title to $AFUS_TITLE
-  replace _AFUS_TITLE_ "$AFUS_TITLE"
-else
-  replace _AFUS_TITLE_ "$DEFAULT_AFUS_TITLE"
+  USE_AFUS_TITLE="$AFUS_TITLE"
 fi
+replace _AFUS_TITLE_ "$USE_AFUS_TITLE"
 
 if [ -n "$AFUS_SUBTITLE" ]; then
   echo Set subtitle to $AFUS_SUBTITLE
-  replace _AFUS_SUBTITLE_ "$AFUS_SUBTITLE"
-else
-  replace _AFUS_SUBTITLE_ "$DEFAULT_AFUS_SUBTITLE"
+  USE_AFUS_SUBTITLE="$AFUS_SUBTITLE"
 fi
+replace _AFUS_SUBTITLE_ "$USE_AFUS_SUBTITLE"
--- web/src/config.ts
+++ web/src/config.ts
@@ -4,8 +4,8 @@ const config: {
   AFUS_SUBTITLE: string;
 } = {
   AFUS_URL: import.meta.env.VITE_AFUS_URL || window.location.origin,
-  AFUS_TITLE: import.meta.env.VITE_AFUS_TITLE || "_AFUS_TITLE_",
-  AFUS_SUBTITLE: import.meta.env.VITE_AFUS_SUBTITLE || "_AFUS_SUBTITLE_",
+  AFUS_TITLE: import.meta.env.VITE_AFUS_TITLE,
+  AFUS_SUBTITLE: import.meta.env.VITE_AFUS_SUBTITLE,
 };
 
 export default config;
